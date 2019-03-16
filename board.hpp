a3360e6043de4620989f338e2fb5f14fe2633adf
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2019-03-16 18:07:06 +0100

    Noncopyable Board & Stone.

--- board.hpp
+++ board.hpp
@@ -10,7 +10,12 @@ constexpr static std::size_t BOARD_WIDTH = 15;
 class Board {
 public:
     using CellsContainerType = std::array<std::array<std::optional<Stone>, BOARD_WIDTH>, BOARD_WIDTH>;
+
     Board();
+    // noncopyable
+    Board(const Board&) = delete;
+    Board& operator = (const Board&) = delete;
+
     void dropStone(const Stone& stone, const Position& position);
     void show() const;
 private:
--- stone.hpp
+++ stone.hpp
@@ -5,6 +5,10 @@
 class Stone {
 public:
     explicit Stone(StoneColor color);
+    // noncopyable
+    Stone(const Stone&) = delete;
+    Stone& operator = (const Stone&) = delete;
+
     StoneColor getColor() const;
 private:
     const StoneColor color;
