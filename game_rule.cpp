effc8d97a0ce6ad856da846938dd9dafbff57787
Tomasz Jasiukiewicz <t,jasiukiewicz@gmail.com>
2019-04-08 10:47:29 +0200

    Add left and right diagonal check

--- game_rule.cpp
+++ game_rule.cpp
@@ -7,7 +7,7 @@
 
 namespace {
 
-constexpr static std::size_t CheckRange = 3;
+constexpr static std::size_t CheckRange = 5;
 
 std::size_t minIndex(std::size_t index) {
     return index < (CheckRange - 1) ? 0: index - (CheckRange - 1);
@@ -100,5 +100,53 @@ bool GameRule::winOnPosition(const Position& position) const {
         return false;
     };
 
-    return checkVertical(position) || checkHorizontal(position);
+
+    auto checkRightDiagonal = [this](const Position& position) {
+        auto buildRightDiagonal = [this](const Position& position) {
+            auto [col, row] = position.getIndexes();
+            auto startDeltaIndex = std::min(col - minIndex(col), row - minIndex(row));
+            auto endDeltaIndex = std::min(maxIndex(col) - col, maxIndex(row) - row);
+            std::vector<const BoardCellsContainerType::value_type::value_type *> ptrCells;
+            for(auto iCol = col - startDeltaIndex, iRow = row - startDeltaIndex;
+                iCol < col + endDeltaIndex; ++iCol, ++iRow) {
+                ptrCells.emplace_back(&((*(this->cells))[iRow][iCol]));
+            }
+            return ptrCells;
+        };
+
+        auto ptrCells = buildRightDiagonal(position);
+        if(find_adjacent_count(ptrCells.cbegin(), ptrCells.cend(), CheckRange,
+            [](const auto& first, const auto& second) {
+                return (*first && *second) ? *(*first) == *(*second): false;
+            }) != ptrCells.cend()) {
+            return true;
+        }
+        return false;
+    };
+
+    auto checkLeftDiagonal = [this](const Position& position) {
+        auto buildLeftDiagonal = [this](const Position& position) {
+            auto [col, row] = position.getIndexes();
+            auto startDeltaIndex = std::min(maxIndex(col) - col - 1, row - minIndex(row));
+            auto endDeltaIndex = std::min(col - minIndex(col) , maxIndex(row) - row - 1);
+            std::vector<const BoardCellsContainerType::value_type::value_type *> ptrCells;
+            for(auto iCol = col + startDeltaIndex, iRow = row - startDeltaIndex;
+                iRow < row + endDeltaIndex + 1; --iCol, ++iRow) {
+                ptrCells.emplace_back(&((*(this->cells))[iRow][iCol]));
+            }
+            return ptrCells;
+        };
+
+        auto ptrCells = buildLeftDiagonal(position);
+        if(find_adjacent_count(ptrCells.cbegin(), ptrCells.cend(), CheckRange,
+            [](const auto& first, const auto& second) {
+                return (*first && *second) ? *(*first) == *(*second): false;
+            }) != ptrCells.cend()) {
+            return true;
+        }
+        return false;
+    };
+
+    return checkVertical(position) || checkHorizontal(position)
+      || checkRightDiagonal(position) || checkLeftDiagonal(position);
 }
